18d03d6015d2206589d39c3251488443a7c8bc4c
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2026-08-03 23:36:20 +0200

    add notification host

--- ui/app.tsx
+++ ui/app.tsx
@@ -9,6 +9,7 @@ import { Busy } from "./busy";
 import { Doc } from "./doc";
 import { DialogHost } from "./dialog-host";
 import { Menu } from "./menu";
+import { NotifyHost } from "./notify";
 
 const use_styles = makeStyles({
   app: {
@@ -26,6 +27,7 @@ export function App() {
   return (
     <FluentProvider theme={webLightTheme} className={styles.app}>
       <AppBar title={title} on_menu_click={() => set_menu_open(true)} />
+      <NotifyHost />
       <Doc on_title_change={set_title} />
       <Menu open={menu_open} show_menu={set_menu_open} />
       <DialogHost />
