af1ca55f8caebb9c18b11b7d195488ab0404fb60
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2026-09-03 12:54:31 +0200

    add event_queue_t
    Thread‑safe queue for delivering events to the python core.

---
+++ src/native/win32/event_queue.cc
@@ -0,0 +1,55 @@
+#include "event_queue.h"
+
+#include <utility>
+
+bool event_queue_t::push(std::string event)
+{
+	std::function<void(std::string)> callback;
+	{
+		std::lock_guard lock(mut);
+		if (!q)
+			return false;
+		if (!cb) {
+			q->push_back(std::move(event));
+			return true;
+		}
+		callback = std::move(cb);
+		cb = nullptr;
+	}
+
+	callback(std::move(event));
+	return true;
+}
+
+bool event_queue_t::set_cb(std::function<void(std::string)> callback)
+{
+	std::string event;
+	{
+		std::lock_guard lock(mut);
+		if (!q)
+			return false;
+		if (q->empty()) {
+			cb = std::move(callback);
+			return true;
+		}
+		event = std::move(q->front());
+		q->pop_front();
+	}
+
+	callback(std::move(event));
+	return true;
+}
+
+void event_queue_t::abort(void)
+{
+	callback_t callback;
+	{
+		std::lock_guard lock(mut);
+		q.reset();
+		callback = std::move(cb);
+		cb = nullptr;
+	}
+
+	if (callback)
+		callback({});
+}
